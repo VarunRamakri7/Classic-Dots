90d05a170ef61a8ad1eaa968ffac78384dbeca03
Varun Ramakrishnan <ramakri7@purdue.edu>
2022-02-03 18:05:19 -0500

    Destroy dot on click
    - Placeholder
    - Repopulate grid on key press

--- Assets/Scripts/S_GameManager.cs
+++ Assets/Scripts/S_GameManager.cs
@@ -19,4 +19,45 @@ public class S_GameManager : MonoBehaviour
 
         gridManager.MakeGrid(); // Make dots grid
     }
+
+    private void Update()
+    {
+        CheckMouseHit();
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            gridManager.RepopulateGrid();
+        }
+    }
+
+    private void CheckMouseHit()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                //Select stage    
+                if (hit.transform.gameObject.tag == "dot")
+                {
+                    Debug.Log("Hit: " + hit.transform.gameObject.name);
+
+                    // Get index of dot
+                    string dotName = hit.transform.gameObject.name;
+                    //Debug.Log("Name: " + dotName);
+
+                    char[] trimChar = { 'd', 'o', 't', '(', ')' };
+                    string trimmed = dotName.Trim(trimChar); // Remove all extra chars except ','
+                    //Debug.Log("After trim: " + trimmed);
+
+                    int[] index = { (int)char.GetNumericValue(trimmed[0]), (int)char.GetNumericValue(trimmed[2]) }; // Get numeric value from chars
+                    //Debug.Log("Index: (" + index[0] + ", " + index[1] + ")");
+
+                    gridManager.RemoveDot(index[0], index[1]); // Remove dot at location
+                }
+            }
+        }
+    }
 }
