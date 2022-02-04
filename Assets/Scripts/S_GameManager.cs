3ad8bfdaf0dc72c148b4e4766f877eb336269e23
Varun Ramakrishnan <ramakri7@purdue.edu>
2022-02-04 18:35:50 -0500

    Added color connection
    - Mouse hold and release are added but unused

--- Assets/Scripts/S_GameManager.cs
+++ Assets/Scripts/S_GameManager.cs
@@ -8,6 +8,11 @@ public class S_GameManager : MonoBehaviour
 
     [SerializeField]
     private S_DotGridManager gridManager;
+    [SerializeField]
+    private S_ConnectDots connectionManager;
+
+    private char[] trimChar = { 'd', 'o', 't', '(', ')' };
+    private List<int[]> dotsIndices;
 
     // Start is called before the first frame update
     void Start()
@@ -16,45 +21,87 @@ public class S_GameManager : MonoBehaviour
         {
             gameManager = this;
         }
+
+        dotsIndices = new List<int[]>();
     }
 
     private void Update()
     {
-        CheckMouseHit();
         CheckKeyPress();
+
+        CheckMousePress();
+        //CheckMouseHold();
     }
 
     /// <summary>
     /// Check Mouse right click
     /// </summary>
-    private void CheckMouseHit()
+    private void CheckMousePress()
     {
-        if (Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
             if (Physics.Raycast(ray, out hit))
             {
-                //Select stage    
+                // Check if a dot is hit
                 if (hit.transform.gameObject.tag == "dot")
                 {
-                    Debug.Log("Hit: " + hit.transform.gameObject.name);
+                    //Debug.Log("Hit: " + hit.transform.gameObject.name);
 
                     // Get index of dot
                     string dotName = hit.transform.gameObject.name;
                     //Debug.Log("Name: " + dotName);
 
-                    char[] trimChar = { 'd', 'o', 't', '(', ')' };
                     string trimmed = dotName.Trim(trimChar); // Remove all extra chars except ','
                     //Debug.Log("After trim: " + trimmed);
 
                     int[] index = { (int)char.GetNumericValue(trimmed[0]), (int)char.GetNumericValue(trimmed[2]) }; // Get numeric value from chars
                     //Debug.Log("Index: (" + index[0] + ", " + index[1] + ")");
 
-                    gridManager.RemoveDot(index[0], index[1]); // Remove dot at location
+                    //gridManager.RemoveDot(index[0], index[1]); // Remove dot at location
+
+                    // TODO: Check color of new dot
+
+                    // Add unique indices
+                    if (!dotsIndices.Contains(index))
+                    {
+                        Debug.Log(string.Format("Adding: ({0}, {1})", index[0], index[1]));
+                        dotsIndices.Add(index); // Add dot index to list
+                    }
+
+                    connectionManager.SetLineColor(hit.transform.gameObject.GetComponent<Renderer>().material.color); // Change line color
+
+                    // Add position to line
+                    Vector3 point = hit.transform.position;
+                    point.y = 0.2f;
+                    connectionManager.AddPoint(point);
                 }
             }
+        }
+    }
+
+    /// <summary>
+    /// Check if mouse is being held
+    /// </summary>
+    public void CheckMouseHold()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            // Change last position to mouse position
+            connectionManager.SetPoint(connectionManager.GetLength(), Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        }
+    }
+
+    /// <summary>
+    /// Check if mouse is released
+    /// </summary>
+    public void CheckMouseRelease()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            // Destroy dots added to list and erase line
         }
     }
 
@@ -66,6 +113,21 @@ public class S_GameManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.R))
         {
             gridManager.RepopulateGrid();
+        }
+        
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            connectionManager.EmptyLine(); // Erase line
+
+            // Iterate through indices and remove all connected dots
+            for(int i = 0; i < dotsIndices.Count; i++)
+            {
+                Debug.Log(string.Format("Removing: ({0}, {1})", dotsIndices[i][0], dotsIndices[i][1]));
+                gridManager.MakeDotsFall(dotsIndices[i][0], dotsIndices[i][1]); // Remove dot
+            }
+            dotsIndices = new List<int[]>(); // Reset list
+
+            //gridManager.RepopulateGrid();
         }
     }
 }
