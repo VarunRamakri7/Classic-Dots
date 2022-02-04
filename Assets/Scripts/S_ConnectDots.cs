e7063bce84090f05946c28afed2c8c3d4360fa5f
Varun Ramakrishnan <ramakri7@purdue.edu>
2022-02-04 15:31:27 -0500

    Renamed script
    - Removed unused code

---
+++ Assets/Scripts/S_ConnectDots.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S_ConnectDots : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        CheckMouseHit();
+    }
+
+    private void CheckMouseHit()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+        }
+    }
+}
